3b5f062580f394e65ee8f027e79ff73ba06368fc
Николай Славнов <n.slavnov@kod-it.ru>
2022-09-25 21:03:43 +0300

    very hot fix

--- web/src/pages/auth/ChangePassword.tsx
+++ web/src/pages/auth/ChangePassword.tsx
@@ -1,47 +1,48 @@
 import {
-    Box,
-    Heading,
-    Text,
-    useToast,
-  } from "@chakra-ui/react";
+  Box,
+  Heading,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
   
-  import Card from "./components/Card";
-  import Logo from "./components/Logo";
-  import ChangePasswordForm from "./components/ChangePasswordForm";
+import Card from "./components/Card";
+import Logo from "./components/Logo";
+import ChangePasswordForm from "./components/ChangePasswordForm";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import Page404 from "pages/404/Page404";
   
-  const ChangePassword = () => {
-    const toast = useToast();
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const token = searchParams.get("token")
+const ChangePassword = () => {
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token")
 
-    if (!token) {
-      navigate('/signin', { replace: true });
-      toast({
-        title: 'Empty token',
-        description: 'Please, try to reset email again',
-        status: "error",
-        isClosable: true,
-      });
+  if (!token) {
+    navigate('/signin', { replace: true });
+    toast({
+      title: 'Empty token',
+      description: 'Please, try to reset email again',
+      status: "error",
+      isClosable: true,
+    });
 
-      return
-    }
+    return Page404()
+  }
 
-    return (<Box bg="pink.100" minH="100vh" py="12" px={{ base: "4", lg: "8" }}>
-      <Box maxW="md" mx="auto">
-        <Logo mx="auto" h="8" mb={{ base: "10", md: "20" }} />
-        <Heading textAlign="center" size="xl" fontWeight="extrabold">
-          Reset password
-        </Heading>
-        <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
-          <Text as="span">Set your new password</Text>
-        </Text>
-        <Card>
-          <ChangePasswordForm token={ token }/>
-        </Card>
-      </Box>
+  return (<Box bg="pink.100" minH="100vh" py="12" px={{ base: "4", lg: "8" }}>
+    <Box maxW="md" mx="auto">
+      <Logo mx="auto" h="8" mb={{ base: "10", md: "20" }} />
+      <Heading textAlign="center" size="xl" fontWeight="extrabold">
+        Reset password
+      </Heading>
+      <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
+        <Text as="span">Set your new password</Text>
+      </Text>
+      <Card>
+        <ChangePasswordForm token={ token }/>
+      </Card>
     </Box>
-  )};
-  
-  export default ChangePassword;
+  </Box>
+)};
+
+export default ChangePassword;
